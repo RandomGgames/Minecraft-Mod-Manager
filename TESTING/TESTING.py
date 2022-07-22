0009c96fb26c3c4872f569c525de1f16f5c3c37f
Gianluca O <gianopp@gmail.com>
2022-07-22 16:57:19 -0400

    Changed Instances
    Moved from folder of instances to a single file.

--- TESTING/TESTING.py
+++ TESTING/TESTING.py
@@ -1,13 +1,27 @@
-import os
 import yaml
+import json
+
+def load_instances():
+	with open("RMMUDInstances.yaml", "r") as f:
+		instances = yaml.safe_load(f)
+		f.close()
+	for instance_name, instance in instances.items():
+		print(f"{instance_name = }")
+		print(json.dumps(instance, indent = "\t"))
+		print()
+		
+		#print(f"{instance} Enabled: {instances[instance]['Enabled']}")
+		#print(type(instances[instance]["Enabled"]))
+		#if instances[instance]["Enabled"]:
+		#	print(instances[instance])
 
 def main():
 	with open("RMMUDConfig.yaml", "r") as f: config = yaml.safe_load(f)
-	print(config)
-	
-	for instance in [file for file in os.listdir("RMMUDInstances") if file.endswith(".yaml")]:
-		with open(f"RMMUDInstances/{instance}", "r") as f: config = yaml.safe_load(f)
-		print(config)
+	download_mods_location = config["Download Mods Location"]
+	check_for_updates = config["Check for RMMUD Updates"]
+	headers = {"Accept": "application/json", "x-api-key": config["CurseForge API Key"]}
+
+	mods = load_instances()
 
 if __name__ == "__main__":
 	main()
